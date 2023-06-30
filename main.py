09db0e87497aab8d9eb285cd19554789c7881757
Benjamin Zeisberg <benjaminzeisberg@college.harvard.edu>
2023-06-30 23:50:24 +0200

    updated files

---
+++ main.py
@@ -0,0 +1,5 @@
+# main file from where we send requests to API
+
+# --> url
+
+URL = 'https://archive-api.open-meteo.com/v1/archive?latitude=52.52&longitude=13.41&start_date=2023-06-04&end_date=2023-06-18&hourly=temperature_2m'
